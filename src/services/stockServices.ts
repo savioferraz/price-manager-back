3c129a52c1ed991700ae073d1cf282c85f222a45
savioferraz <savio.ferraz88@gmail.com>
2023-05-13 21:13:44 -0300

    feat: add price validation structure

--- src/services/stockServices.ts
+++ src/services/stockServices.ts
@@ -2,10 +2,39 @@ import stockRepository from "@/repositories/stockRepository";
 import { formatCSV } from "@/utils/formatCSV";
 
 async function validatePrices(csvFile: any) {
-  const prices = await stockRepository.getPrices();
+  const products = await stockRepository.getPrices();
   const parsedCsv = formatCSV(csvFile);
 
   const validateProducts = [];
+
+  for (const csvProduct of parsedCsv) {
+    const { product_code, new_price } = csvProduct;
+
+    const dbProduct = products.find((product) => product.code === product_code);
+
+    const salesPrice = Number(dbProduct.sales_price);
+    const costPrice = Number(dbProduct.cost_price);
+    const newPrice = Number(new_price);
+
+    if (newPrice < costPrice) {
+      validateProducts.push({ product_code: product_code, new_price: newPrice, status: "Abaixo do preço de custo" });
+    }
+    if (newPrice > salesPrice * 1.1 || newPrice < salesPrice * 0.9) {
+      validateProducts.push({
+        product_code: product_code,
+        new_price: newPrice,
+        status: "Não pode ser maior ou menor que 10%",
+      });
+    } else {
+      validateProducts.push({
+        product_code: product_code,
+        new_price: newPrice,
+        status: "OK",
+      });
+    }
+
+    return validateProducts;
+  }
 
   return "ok";
 }
